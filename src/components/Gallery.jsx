d6e5a9a7f8ed1f8cff96b135624883743145f255
cwitmer34 <connorwitmer34@gmail.com>
2022-09-20 18:11:45 -0400

    fetching and displaying data

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+import GalleryItem from "./GalleryItem"
+
+const Gallery = (props) => {
+  const display = props.data.map((item, index) => {
+    return <GalleryItem item={item} key={index} />
+  })
+  return (
+    <div>
+      {display}
+    </div>
+  )
+}
+
+export default Gallery
