b21455cad6993bbb9bb9b4780e08000b15c45ab9
cwitmer34 <connorwitmer34@gmail.com>
2022-10-04 12:24:02 -0400

    Router working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
 import "./App.css";
 
 function App() {
@@ -33,9 +36,22 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Fragment>
+                <SearchBar handleSearch={handleSearch} />
+                <Gallery data={data} />
+              </Fragment>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
